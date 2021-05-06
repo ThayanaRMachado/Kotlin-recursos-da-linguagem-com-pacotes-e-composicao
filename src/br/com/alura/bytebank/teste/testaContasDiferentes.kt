44707f5644532db6d803149c9657370ec36e95f3
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-06 11:32:45 -0300

    2.3 - Criando o endereço do cliente

--- src/br/com/alura/bytebank/teste/testaContasDiferentes.kt
+++ src/br/com/alura/bytebank/teste/testaContasDiferentes.kt
@@ -1,13 +1,17 @@
 import br.com.alura.bytebank.modelo.Cliente
 import br.com.alura.bytebank.modelo.ContaCorrente
 import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.Endereco
 
 fun testaContasDiferentes() {
     val contaCorrente = ContaCorrente(
         titular = Cliente(
             nome = "Alex",
             cpf = "111.111.111-11",
-            senha = 1
+            senha = 1,
+            endereco = Endereco(
+                logradouro = "Rua A"
+            )
         ),
         numero = 1000
     )
@@ -43,6 +47,9 @@ fun testaContasDiferentes() {
     println("saldo corrente após receber transferência: ${contaCorrente.saldo}")
 
     println("Titular")
+    println("Nome do titular ${contaCorrente.titular}")
     println("Nome do titular ${contaCorrente.titular.nome}")
     println("CPF do titular ${contaCorrente.titular.cpf}")
+    println("Endereço titular ${contaCorrente.titular.endereco.logradouro}")
+    println("Endereço titular ${contaCorrente.titular.endereco}")
 }
