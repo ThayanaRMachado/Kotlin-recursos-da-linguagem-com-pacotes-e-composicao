6c44743d4fe8d380d2f791f7fedace19d487863c
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-06 15:41:08 -0300

    4.3 - Testando a classe Any

--- src/main.kt
+++ src/main.kt
@@ -1,37 +1,16 @@
-import br.com.alura.bytebank.modelo.Cliente
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    println()
-    println(1)
-    println(1.0)
-    println(true)
-    println("teste")
-
     val endereco = Endereco()
+    val objeto = Any = Any()
 
-    println(endereco)
-    println(Cliente())
+    imprime(objeto)
 
-    imprime()
+    val teste: Any = imprime(endereco)
+    println(teste)
 
-    imprime(1)
-
-    imprime(1.0)
-
-    imprime(endereco)
-
-    fun imprime(){
-
-    }
-    fun imprime(valor: Int){
-
-    }
-    fun imprime(valor: Double){
-
-    }
-
-    fun imprime(endereco: Endereco){
-
+    fun imprime(valor: Any) : Any{
+        println(valor)
+        return valor
     }
 }
